6dee4a3a58e560fbb60a363cdf6a166b7a9f7050
Leo Tostain <leo.tostain@gmail.com>
2023-05-18 16:52:31 +0200

    Add taskAccepted

---
+++ ugp-protocol/src/main/java/fr/networks/ugp/readers/TaskAcceptedReader.java
@@ -0,0 +1,37 @@
+package fr.networks.ugp.readers;
+
+import fr.networks.ugp.data.TaskId;
+
+import java.nio.ByteBuffer;
+
+public class TaskAcceptedReader implements Reader<TaskId>{
+  private enum State { DONE, REFILL, ERROR }
+  private State state = State.REFILL;
+  private final TaskIdReader taskIdReader = new TaskIdReader();
+  private TaskId taskId;
+  public ProcessStatus process(ByteBuffer bb) {
+    if (state == State.DONE || state == State.ERROR) {
+      throw new IllegalStateException();
+    }
+
+    var status = taskIdReader.process(bb);
+    if(status != ProcessStatus.DONE) {
+      return status;
+    }
+    taskId = taskIdReader.get();
+    state = State.DONE;
+    return ProcessStatus.DONE;
+  }
+
+  public TaskId get() {
+    if (state != State.DONE) {
+      throw new IllegalStateException();
+    }
+    return taskId;
+  }
+
+  public void reset() {
+    taskIdReader.reset();
+    state = State.REFILL;
+  }
+}
---
+++ ugp-protocol/src/test/java/fr/networks/ugp/readers/TaskAcceptedReaderTest.java
@@ -0,0 +1,27 @@
+package fr.networks.ugp.readers;
+
+import fr.networks.ugp.data.TaskId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+
+public class TaskAcceptedReaderTest {
+  private TaskAcceptedReader taskAcceptedReader;
+
+  @BeforeEach
+  void setUp() {
+    taskAcceptedReader = new TaskAcceptedReader();
+  }
+
+  @Test
+  void simple() {
+    long id = 123456789L;
+    InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8080);
+    TaskId expectedTaskId = new TaskId(id, address);
+
+    Assertions.assertEquals(Reader.ProcessStatus.DONE, taskAcceptedReader.process(expectedTaskId.encode()));
+    Assertions.assertEquals(expectedTaskId, taskAcceptedReader.get());
+  }
+}
