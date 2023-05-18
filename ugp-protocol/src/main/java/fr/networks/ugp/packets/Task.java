d236b68e259788d75870ada8d665f22a0cf7b364
Mick <bmikdaam@gmail.com>
2023-05-18 18:50:11 +0200

    - Correct TaskId encode

--- ugp-protocol/src/main/java/fr/networks/ugp/packets/Task.java
+++ ugp-protocol/src/main/java/fr/networks/ugp/packets/Task.java
@@ -12,21 +12,24 @@ public record Task(TaskId id, URL url, String className, Range range) implements
     public ByteBuffer encode() {
         var taskIdBuffer = id.encode();
         var rangeBuffer = range.encode();
-        var urlString = url.toString();
+        var urlBuffer = StandardCharsets.UTF_8.encode(url.toString());
+        var classNameBuffer = StandardCharsets.UTF_8.encode(className);
 
         taskIdBuffer.flip();
         rangeBuffer.flip();
+        urlBuffer.flip();
+        classNameBuffer.flip();
 
         var buffer = ByteBuffer.allocate(taskIdBuffer.remaining()
-                + Integer.BYTES + urlString.length()
-                + Integer.BYTES + className.length()
+                + Integer.BYTES + urlBuffer.remaining()
+                + Integer.BYTES + classNameBuffer.remaining()
                 + rangeBuffer.remaining());
 
         buffer.put(taskIdBuffer);
-        buffer.putInt(urlString.length());
-        buffer.put(StandardCharsets.UTF_8.encode(urlString));
-        buffer.putInt(className.length());
-        buffer.put(StandardCharsets.UTF_8.encode(className));
+        buffer.putInt(urlBuffer.remaining());
+        buffer.put(urlBuffer);
+        buffer.putInt(classNameBuffer.remaining());
+        buffer.put(classNameBuffer);
         buffer.put(rangeBuffer);
 
         return buffer;
