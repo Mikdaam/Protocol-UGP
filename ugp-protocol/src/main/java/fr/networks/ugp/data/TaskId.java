07d73b5d4ef30d9fb4fe28f868f38a4d5cd136ee
Mick <bmikdaam@gmail.com>
2023-05-18 15:48:42 +0200

    - Update encode in TaskId

--- ugp-protocol/src/main/java/fr/networks/ugp/data/TaskId.java
+++ ugp-protocol/src/main/java/fr/networks/ugp/data/TaskId.java
@@ -2,12 +2,16 @@ package fr.networks.ugp.data;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.util.Map;
 
 public record TaskId(long id, InetSocketAddress socketAddress) {
-    private static final ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES + 16);
+    private static final Map<Integer, Integer> versionToBytes = Map.of(4, 4, 6, 16);
     public ByteBuffer encode() {
         var address = socketAddress.getAddress().getAddress();
         int version = address.length;
+        int nbOfBytes = versionToBytes.get(version);
+
+        var buffer = ByteBuffer.allocate(Long.BYTES + 1 + nbOfBytes + Integer.BYTES);
         buffer.putLong(id);
         buffer.put((byte) version);
         buffer.put(address);
