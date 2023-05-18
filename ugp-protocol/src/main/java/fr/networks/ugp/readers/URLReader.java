680535c0135316536e70bd838060c8434073764c
Leo Tostain <leo.tostain@gmail.com>
2023-05-18 13:29:55 +0200

    Add url test

--- ugp-protocol/src/main/java/fr/networks/ugp/readers/URLReader.java
+++ ugp-protocol/src/main/java/fr/networks/ugp/readers/URLReader.java
@@ -3,16 +3,17 @@ package fr.networks.ugp.readers;
 import fr.networks.ugp.readers.base.StringReader;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 
-public class URLReader implements Reader<URL> {
+public class URLReader implements Reader<URI> {
     private enum State {
         DONE, WAITING_CONTENT, ERROR
     }
 
     private State state = State.WAITING_CONTENT;
-    private URL url = null;
+    private URI uri = null;
     private final StringReader stringReader = new StringReader();
 
     @Override
@@ -29,23 +30,27 @@ public class URLReader implements Reader<URL> {
             return status;
         }
         var string = stringReader.get();
-        /*if(!string.matches("^https:\\/\\/.*")) {
-            return ProcessStatus.ERROR;
-        }*/
-        try {
-            url = new URL(string);
-        } catch (MalformedURLException e) {
+        if(!string.matches("^http://.*")) {
             return ProcessStatus.ERROR;
         }
+        try {
+            uri = new URI(string);
+        } catch (URISyntaxException e) {
+            state = State.ERROR;
+            return ProcessStatus.ERROR;
+        }
+        System.out.println(uri.getScheme());
+        System.out.println(uri.getHost());
+        state = State.DONE;
         return ProcessStatus.DONE;
     }
 
     @Override
-    public URL get() {
+    public URI get() {
         if (state != State.DONE) {
             throw new IllegalStateException();
         }
-        return url;
+        return uri;
     }
 
     @Override
---
+++ ugp-protocol/src/test/java/fr/networks/ugp/readers/URLReaderTest.java
@@ -0,0 +1,83 @@
+package fr.networks.ugp.readers;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class URLReaderTest {
+  @Test
+  public void simple() throws URISyntaxException {
+    var string = "http://docs.google.com/document/d/1AjTmTtZMehO35vIylUnCvPO2X7mu7iffD0P4Tbwwfwc/edit#";
+    var bb = ByteBuffer.allocate(1024);
+    var bytes = StandardCharsets.UTF_8.encode(string);
+    bb.putInt(bytes.remaining()).put(bytes);
+    var sr = new URLReader();
+    assertEquals(Reader.ProcessStatus.DONE, sr.process(bb));
+    assertEquals(new URI(string), sr.get());
+    assertEquals(0, bb.position());
+    assertEquals(bb.capacity(), bb.limit());
+  }
+
+  @Test
+  public void reset() throws URISyntaxException {
+    var string = "http://docs.google.com/document/d/1AjTmTtZMehO35vIylUnCvPO2X7mu7iffD0P4Tbwwfwc/edit#";
+    var string2 = "http://google.com";
+    var bb = ByteBuffer.allocate(1024);
+    var bytes = StandardCharsets.UTF_8.encode(string);
+    var bytes2 = StandardCharsets.UTF_8.encode(string2);
+    bb.putInt(bytes.remaining()).put(bytes).putInt(bytes2.remaining()).put(bytes2);
+    var sr = new URLReader();
+    assertEquals(Reader.ProcessStatus.DONE, sr.process(bb));
+    assertEquals(new URI(string), sr.get());
+    assertEquals(bb.capacity(), bb.limit());
+    sr.reset();
+    assertEquals(Reader.ProcessStatus.DONE, sr.process(bb));
+    assertEquals(new URI(string2), sr.get());
+    assertEquals(0, bb.position());
+    assertEquals(bb.capacity(), bb.limit());
+  }
+
+  @Test
+  public void reffil() throws URISyntaxException {
+    var string = "http://docs.google.com/document/d/1AjTmTtZMehO35vIylUnCvPO2X7mu7iffD0P4Tbwwfwc/edit#";
+    var bb = ByteBuffer.allocate(1024);
+    var bytes = StandardCharsets.UTF_8.encode(string);
+    bb.putInt(bytes.remaining()).put(bytes);
+    var ur = new URLReader();
+    bb.flip();
+    var smallBuffer = ByteBuffer.allocate(4);
+    var status = Reader.ProcessStatus.REFILL;
+    while(bb.hasRemaining()) {
+      var oldLimit = bb.limit();
+      bb.limit(bb.position() + smallBuffer.capacity());
+      smallBuffer.put(bb);
+      bb.limit(oldLimit);
+
+      status = ur.process(smallBuffer);
+      if(bb.hasRemaining()) {
+        assertEquals(Reader.ProcessStatus.REFILL, status);
+      }
+      smallBuffer.clear();
+    }
+    assertEquals(Reader.ProcessStatus.DONE, status);
+    assertEquals(new URI(string), ur.get());
+    assertEquals(0, smallBuffer.position());
+    assertEquals(smallBuffer.capacity(), smallBuffer.limit());
+  }
+
+  @Test
+  public void wrongFormat() {
+    var string = "http:/google.com";
+    var bb = ByteBuffer.allocate(1024);
+    var bytes = StandardCharsets.UTF_8.encode(string);
+    bb.putInt(bytes.remaining()).put(bytes);
+    var sr = new URLReader();
+    assertEquals(Reader.ProcessStatus.ERROR, sr.process(bb));
+  }
+}
