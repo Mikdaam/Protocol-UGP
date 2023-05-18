e02858a8015e2043021d5ff36e37b4dc0d1d7888
Leo Tostain <leo.tostain@gmail.com>
2023-05-18 15:53:10 +0200

    Add range tests

--- ugp-protocol/src/test/java/fr/networks/ugp/readers/RangeReaderTest.java
+++ ugp-protocol/src/test/java/fr/networks/ugp/readers/RangeReaderTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.nio.ByteBuffer;
+
 public class RangeReaderTest {
     private RangeReader rangeReader;
 
@@ -18,6 +20,37 @@ public class RangeReaderTest {
         var expectedRange = new Range(0L, 48523L);
 
         Assertions.assertEquals(Reader.ProcessStatus.DONE, rangeReader.process(expectedRange.encode()));
+        Assertions.assertEquals(expectedRange, rangeReader.get());
+    }
+
+    @Test
+    public void negativeLong() {
+        var expectedRange = new Range(-1L, 48523L);
+
+        Assertions.assertEquals(Reader.ProcessStatus.ERROR, rangeReader.process(expectedRange.encode()));
+    }
+
+    @Test
+    public void toInferiorFrom() {
+        var expectedRange = new Range(500L, 15L);
+
+        Assertions.assertEquals(Reader.ProcessStatus.ERROR, rangeReader.process(expectedRange.encode()));
+    }
+
+    @Test
+    public void smallBuffer() {
+        var expectedRange = new Range(0L, 48523L);
+        var rangeBuffer = expectedRange.encode();
+        var smallBuffer = ByteBuffer.allocate(Long.BYTES);
+
+        rangeBuffer.flip();
+        smallBuffer.putLong(rangeBuffer.getLong());
+        Assertions.assertEquals(Reader.ProcessStatus.REFILL, rangeReader.process(smallBuffer));
+
+        smallBuffer.clear();
+        smallBuffer.putLong(rangeBuffer.getLong());
+        Assertions.assertEquals(Reader.ProcessStatus.DONE, rangeReader.process(smallBuffer));
+
         Assertions.assertEquals(expectedRange, rangeReader.get());
     }
 }
