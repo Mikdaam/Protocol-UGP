20e752baa79c821f74f03088770f63e8266f1462
Leo Tostain <leo.tostain@gmail.com>
2023-05-24 13:03:59 +0200

    Update reconnect reader

--- ugp-protocol/src/main/java/fr/networks/ugp/readers/packets/ReconnectReader.java
+++ ugp-protocol/src/main/java/fr/networks/ugp/readers/packets/ReconnectReader.java
@@ -1,6 +1,7 @@
 package fr.networks.ugp.readers.packets;
 
 import fr.networks.ugp.data.TaskId;
+import fr.networks.ugp.packets.NotifyChild;
 import fr.networks.ugp.packets.Packet;
 import fr.networks.ugp.packets.Reconnect;
 import fr.networks.ugp.readers.Reader;
@@ -11,67 +12,16 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class ReconnectReader implements Reader<Packet> {
-    private enum State {
-        DONE, WAITING_NUMBER, WAITING_IDS, ERROR
-    }
-
-    private State state = State.WAITING_NUMBER;
-    private final TaskIdReader taskIdReader = new TaskIdReader();
-    private final IntReader intReader = new IntReader();
-    private Reconnect reconnect;
-    private ArrayList<TaskId> taskIdList;
-    private int nbOfIds;
     @Override
     public ProcessStatus process(ByteBuffer bb) {
-        if (state == State.DONE || state == State.ERROR) {
-            throw new IllegalStateException();
-        }
-
-        if (state == State.WAITING_NUMBER) {
-            var status = intReader.process(bb);
-            if (status == ProcessStatus.DONE) {
-                state = State.WAITING_IDS;
-                nbOfIds = intReader.get();
-                if (nbOfIds <= 0) {
-                    return ProcessStatus.ERROR;
-                }
-                taskIdList = new ArrayList<>(nbOfIds);
-                intReader.reset();
-            } else {
-                return status;
-            }
-        }
-
-        while(state == State.WAITING_IDS) {
-            var status = taskIdReader.process(bb);
-            if (status != ProcessStatus.DONE) {
-                return status;
-            } else {
-                taskIdList.add(taskIdReader.get());
-                taskIdReader.reset();
-                if (taskIdList.size() == nbOfIds) {
-                    reconnect = new Reconnect(taskIdList);
-                    state = State.DONE;
-                    return ProcessStatus.DONE;
-                }
-            }
-        }
-
-        throw new AssertionError();
+        return ProcessStatus.DONE;
     }
 
     @Override
-    public Reconnect get() {
-        if (state != State.DONE) {
-            throw new IllegalStateException();
-        }
-        return reconnect;
+    public Packet get() {
+        return new Reconnect();
     }
 
     @Override
-    public void reset() {
-        state = State.WAITING_NUMBER;
-        intReader.reset();
-        taskIdReader.reset();
-    }
+    public void reset() {}
 }
